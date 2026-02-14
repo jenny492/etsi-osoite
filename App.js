fedffa3e2b0ec3b1b607c96cab1b4d815d779fb2
Jenny <205511984+jenny492@users.noreply.github.com>
2026-02-14 18:48:52 +0200

    Sovellus näyttää kartan ja aloituspisteen, tekstikentän ja buttonin.Muotoilut ok, kartta menee yläosasta View:n verran näytön yli.

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// ehkä tämä jos ehtii: https://kirillzyusko.github.io/react-native-keyboard-controller/docs/installation
+
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [address, setAddress] = useState('');
+  const [latitude, setLatitude] = useState(60.200692);
+  const [longitude, setLongitude] = useState(24.934302);
+
+  const coordinates = {
+    latitude: Number(latitude),
+    longitude: Number(longitude),
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221,
+};
+  
+  const showOnMap = () => {
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={{ width: '100%', height: '100%', flex: 1 }}
+        region={coordinates}>
+        <Marker
+          coordinate={coordinates} />
+      </MapView>
+      <View style={{ paddingBottom: 60, paddingTop: 20, alignContent: 'center', justifyContent: 'center', width: '80%' }}>
+      <TextInput
+        placeholder="Enter address"
+        value={address}
+        onChangeText={setAddress}
+      />
+      <Button title="Show" onPress={showOnMap} />
+      </View>
     </View>
   );
 }
