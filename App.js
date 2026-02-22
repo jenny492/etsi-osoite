ff793ee2d2b9551f6082a4dcadcdd8f8351f483a
Jenny <205511984+jenny492@users.noreply.github.com>
2026-02-22 16:24:57 +0200

    Lisätty sovellukseen nykyisen sijainnin paikannus sovelluksen avauksen yhteydessä.

--- App.js
+++ App.js
@@ -1,34 +1,45 @@
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Alert, Button, StyleSheet, TextInput, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
 
   const [address, setAddress] = useState('');
-
   const defaultCoordinates = {
     latitude: 60.200692,
     longitude: 24.934302,
     latitudeDelta: 0.0322,
     longitudeDelta: 0.0221,
   };
-
   const [coordinates, setCoordinates] = useState(defaultCoordinates);
 
-  const showOnMap = async () => {
+  const getCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('Permission to access location was denied');
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setCoordinates(prev => ({
+      ...prev,
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    }));
+  }
 
+  useEffect(() => { getCurrentLocation(); }, []);
+
+   const showOnMap = async () => {
     if (!address) {
       return;
     }
-
     const apiKey = process.env.EXPO_PUBLIC_API_KEY;
     const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`;
-
     try {
       const response = await fetch(url);
       const data = await response.json();
-
       // tallennetaan koordinaatit tilaan, jotta kartta päivittyy
       setCoordinates(prev => ({
         ...prev,
@@ -39,8 +50,6 @@ export default function App() {
       console.error('Error fetching geocode data:', error);
     }
   }
-
-  useEffect(() => { showOnMap() }, []);
 
   return (
     <SafeAreaProvider>
