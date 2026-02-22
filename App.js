40bda5dfa827ec3f2d7b90cc730303c9eb717c4b
Jenny <205511984+jenny492@users.noreply.github.com>
2026-02-22 12:47:12 +0200

    Lisätty tietojen hakeminen API:sta ja kartan päivittyminen osoitteen päivittyessä.

--- App.js
+++ App.js
@@ -1,28 +1,50 @@
-// ehkä tämä jos ehtii: https://kirillzyusko.github.io/react-native-keyboard-controller/docs/installation
-
 import { Button, StyleSheet, TextInput, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
 
   const [address, setAddress] = useState('');
-  const [latitude, setLatitude] = useState(60.200692);
-  const [longitude, setLongitude] = useState(24.934302);
 
-  const coordinates = {
-    latitude: Number(latitude),
-    longitude: Number(longitude),
+  const defaultCoordinates = {
+    latitude: 60.200692,
+    longitude: 24.934302,
     latitudeDelta: 0.0322,
     longitudeDelta: 0.0221,
-};
-  
-  const showOnMap = () => {
+  };
 
+  const [coordinates, setCoordinates] = useState(defaultCoordinates);
+
+  const showOnMap = async () => {
+
+    if (!address) {
+      return;
+    }
+
+    const apiKey = process.env.EXPO_PUBLIC_API_KEY;
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      // tallennetaan koordinaatit tilaan, jotta kartta päivittyy
+      setCoordinates(prev => ({
+        ...prev,
+        latitude: data.results[0].geometry.location.lat,
+        longitude: data.results[0].geometry.location.lng,
+      }))
+    } catch (error) {
+      console.error('Error fetching geocode data:', error);
+    }
   }
 
+  useEffect(() => { showOnMap() }, []);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
       <MapView
         style={{ width: '100%', height: '100%', flex: 1 }}
         region={coordinates}>
@@ -30,14 +52,15 @@ export default function App() {
           coordinate={coordinates} />
       </MapView>
       <View style={{ paddingBottom: 60, paddingTop: 20, alignContent: 'center', justifyContent: 'center', width: '80%' }}>
-      <TextInput
-        placeholder="Enter address"
-        value={address}
-        onChangeText={setAddress}
-      />
-      <Button title="Show" onPress={showOnMap} />
+        <TextInput
+          placeholder="Enter address"
+          value={address}
+          onChangeText={setAddress}
+        />
+        <Button title="Show" onPress={showOnMap} />
       </View>
-    </View>
+    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
